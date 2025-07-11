ed83980e1b8f6b33d76ef5a130b667a8b9632bbe
Furkan Aydemir <furkanaydemir12@hotmail.com>
2025-07-11 15:08:42 +0300

    Migrations & Databasew connect start

---
+++ HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-798VB1N\\MSSQLSERVERDEV;Initial Catalog=ApiDb;Integrated Security=True;");
+        }
+
+        // SQL'e yansıyacak olan tablolar
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subcribe> Subcribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
