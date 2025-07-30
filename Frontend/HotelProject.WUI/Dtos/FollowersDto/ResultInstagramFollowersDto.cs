4c5817b3e7a5ea2cba5c136d0dce86ea61c13ccf
Furkan Aydemir <furkanaydemir12@hotmail.com>
2025-07-31 00:35:24 +0300

    Instagram Api ile veirlerin çekilmesi başarılı

---
+++ Frontend/HotelProject.WUI/Dtos/FollowersDto/ResultInstagramFollowersDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.WUI.Dtos.FollowersDto
+{
+    public class ResultInstagramFollowersDto
+    {
+
+        public int followers { get; set; }
+        public int following { get; set; }
+
+    }
+}
--- Frontend/HotelProject.WUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WUI.Dtos.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial : ViewComponent
     {
-
-        public IViewComponentResult Invoke()
+      
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+
+           
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/murattycedag"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a36c6ee4cfmshc9269ffa9c21abfp187d19jsnaed6b4df9064" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+             ResultInstagramFollowersDto   resultInstagramFollowersDtos = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                return View(resultInstagramFollowersDtos);
+            }
+
         }
     }
 }
